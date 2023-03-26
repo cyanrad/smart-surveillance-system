86ee7426f0cf8eefa228292f4e98b7a7bc9da994
phantasmalProxy <60131230+cyanrad@users.noreply.github.com>
2023-03-27 00:02:15 +0400

    changed img_index_to_class from list of tuples to dictionary

--- img_index_to_class.py
+++ img_index_to_class.py
@@ -6,12 +6,10 @@ import const
 #NOTE: this file acts as a singleton
 #NOTE: I hate how i implemented this, but i'm not skilled enough to figure something better
 
-#TODO: refactor this shit as a map
-
 # each image has a unique integer index
 # each image has a class (the identity of the person in the img)
 # this is a map between them
-_data = []
+_data = {}
 
 def save():
     _asset_data_is_not_empty()
@@ -30,13 +28,11 @@ def load_from_data(indexing, identity):
     global _data
     _asset_data_is_empty()
     for i in range(len(indexing)):
-        _data.append((indexing[i], identity[i]))
+        _data[indexing[i]] = identity[i]
 
 def get_class(result_index):
     _asset_data_is_not_empty()
-    for img_index, img_class in _data:
-        if result_index == img_index:
-            return img_class
+    return _data[result_index]
 
 def saved():
     os.path.isfile(const.IMG_INDEX_TO_CLASS_FILE)
