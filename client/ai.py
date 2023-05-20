d77f2c16ac44b9ab2b9ef4949492b6440db676a1
Radwan Jabraouti <radwan.jabraouti@paperworks.ae>
2023-05-21 00:10:30 +0400

    feat(client): video display and fps limiting

--- client/ai.py
+++ client/ai.py
@@ -4,6 +4,7 @@ import requests
 import websockets
 import cv2 as cv
 import authentication
+import math
 
 # loading env uri for posting faces
 HTTP_URI: str
@@ -26,12 +27,36 @@ async def sendVideo(camera: dict, video_name: str) -> None:
     # reading video file
     video = cv.VideoCapture('./videos/' + video_name)
 
+    # we want to send 15 frames per second
+    fps = video.get(cv.CAP_PROP_FPS)
+    ignore_ratio, i = 0, 0
+    if fps > 15:
+        ignore_ratio = math.floor(fps/15)
+    print(ignore_ratio)
+
     async with websockets.connect(WS_URI + "/stream/" + str(camera["ID"])) as ws:
         # reading video frame by frame
         while True:
             ok, frame = video.read()
             if not ok:
                 break
+            frame = cv.resize(frame, (480, 270))
+
+            # displaying the image on a window
+            cv.imshow('stream_' + str(camera["ID"]), frame)
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                break
+
+            # regulating sent frames count to 15 per second for effeciency
+            if not (i == ignore_ratio):
+                print("skipped")
+                i += 1
+                continue
+            else:
+                i = 0
+            print("processed")
+
+            # Wait for the 'q' key to be pressed to exit
 
             # converting bytes to jpg
             _, frame_bytes = cv.imencode(".jpg", frame)
@@ -47,13 +72,13 @@ async def sendVideo(camera: dict, video_name: str) -> None:
             # getting found faces
             respMap = json.loads(respData)
 
-            if len(respMap["detected"]) == 0:
-                print(str(camera["ID"]) + "-no people detected")
+            # if len(respMap["detected"]) == 0:
+            # print(str(camera["ID"]) + "-no people detected")
 
             for detected in respMap["detected"]:
                 # if there is a face detected but unkown
                 if detected == "":
-                    print(str(camera["ID"]) + "-unknown person detected")
+                    # print(str(camera["ID"]) + "-unknown person detected")
                     continue
 
                 person_id_pair = detected.split("_")
@@ -63,11 +88,11 @@ async def sendVideo(camera: dict, video_name: str) -> None:
                     camera["ID"], person_id_pair[1])
 
                 # handling auth conditions
-                if camera["Is_Black_List"] ^ (access is None):
-                    print(str(camera["ID"]) +
-                          "-unauthorized detected: ", detected)
-                else:
-                    print(str(camera["ID"]) + "-detected: ", detected)
+                # if camera["Is_Black_List"] ^ (access is None):
+                # print(str(camera["ID"]) +
+                #       "-unauthorized detected: ", detected)
+                # else:
+                # print(str(camera["ID"]) + "-detected: ", detected)
 
         video.release()
         await ws.close()
